06674773a192fc752ee3b42b6dd71fcea2c16a12
itsmusili001 <musilivictoria9879@gmail.com>
2025-09-19 05:51:08 -0700

    Add files via upload

---
+++ personalInfo.c
@@ -0,0 +1,27 @@
+/*
+NAME:VICTORIA MUTETHYA MUSILI
+REG NO:PA106/G/29037/25
+DESCRIPTION:PROGRAM THAT PROMPTS THE USER TO ENTER AND DISPLAY 
+(HEIGHT,ID NUMBER,BANK BALANCE)
+*/
+
+#include <stdio.h>
+int main()
+{
+	int height,IDNo,BankBalance;
+	printf("Enter your height-");
+	scanf("%d",&height);
+	
+	printf("Enter your Id number-");
+	scanf("%d", &IDNo);
+	
+	printf("enter your bank balance-");
+	scanf("%d", &BankBalance);
+	
+	printf("height: %d\n", height );
+	printf("IDNo: %d\n", IDNo);
+	printf("Bank Balance: %d\n", BankBalance);
+	
+	
+	return 0;
+}
---
+++ units.c
@@ -0,0 +1,22 @@
+/*
+NAME:VICTORIA MUTETHYA MUSILI
+REG NO:PA106/G/29037/25
+DESCRIPTION:PROGRAM TO DISPLAY UNITS TAKEN THIS SEMESTER
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	
+printf("SPC2101-Introduction to programming and problem solving \n");
+printf("SPI2102-Computer Systems and organization \n");
+printf("SPM2100-Discrete Mathematics \n");
+printf("SPM2104-Mathematics for Science \n");
+printf("SSE2101-Fundamentals of System Analysis and Design \n");
+printf("UCU2105-Fundamentals of ICT \n");
+printf("UCU2106-Communication and information Literacy Skills \n");
+
+
+return 0;
+}
